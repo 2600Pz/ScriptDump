32e0c7c5721f025be75091c304d85b12d5222305
2600Pz <105690957+2600Pz@users.noreply.github.com>
2025-07-02 17:45:56 -0600

    Update USMT-Scan.ps1
    Added some checks and error handling

--- USMT-Scan.ps1
+++ USMT-Scan.ps1
@@ -1,12 +1,20 @@
-$user1 = (read-host -prompt "Enter the username of the account to be saved")
-
-$confirm2ndUser = (Read-host -prompt "Do you need to save a second user? (Y/N)")
-
-$usernamesToSave = @($user1)
-
-if ($confirm2ndUser -eq 'Y' -or $confirm2ndUser -eq 'y') {
-    $user2 = (read-host -prompt "Enter the username of the second account to be saved")
-    $usernamesToSave += $user2
+$usernamesToSave = @()
+while ($true) {
+    $username = Read-Host "Enter a username to save (or press Enter to finish)"
+    if ([string]::IsNullOrWhiteSpace($username)) {
+        break
+    }
+    if (test-path -path C:\Users\$username) {
+        continue
+    } else {
+        Write-Host "Username '$username' does not exist. Please enter a valid username."
+        continue
+    }
+    if ($username -notin $usernamesToSave) {
+        $usernamesToSave += $username
+    } else {
+        Write-Host "Username '$username' is already in the list. Please enter a different username."
+    }
 }
 
 $uiArgs = ""
@@ -14,8 +22,36 @@ foreach ($user in $usernamesToSave) {
     $uiArgs += "/ui:$user "
 }
 
-New-PSDrive -name "home" -PSProvider FileSystem -Root '\\fs04\homes$\WatsoJ340' | Out-null
+if (test-path -path home:\usmt) {
+    continue
+} else {
+    New-PSDrive -name "home" -PSProvider FileSystem -Root '\\fs04\homes$\WatsoJ340' | Out-null
+}
 
 Set-Location home:\usmt | Out-Null
 
-.\scanstate.exe \\fs04\homes$\watsoj340\$user1 /i:MigDocs.xml /i:MigUser.xml /ue:*\* $uiArgs /c
+$user1 = $usernamesToSave[0]
+
+if (Test-Path -path \\fs04\homes$\watsoj340\$user1) {
+    $choice = Read-Host -Prompt "The folder $user1 already exists. Do you want to overwrite it? (Y/N)" -foregroundcolor Yellow
+    if ($choice -eq 'Y' -or $choice -eq 'y') {
+        try {
+            .\scanstate.exe \\fs04\homes$\watsoj340\$user1 /i:MigDocs.xml /i:MigUser.xml /ue:*\* $uiArgs /o /c
+            return $true
+        } catch {
+            Write-Host "Error occurred while running scanstate.exe: $_" -ForegroundColor Red
+            return $false
+        }
+    } else {
+        Write-Host "Operation cancelled by user." -ForegroundColor Yellow
+        return $false
+    }
+} else {
+    try {
+        .\scanstate.exe \\fs04\homes$\watsoj340\$user1 /i:MigDocs.xml /i:MigUser.xml /ue:*\* $uiArgs /c
+        return $true
+    } catch {
+        Write-Host "Error occurred while running scanstate.exe: $_" -ForegroundColor Red
+        return $false
+    }
+}
