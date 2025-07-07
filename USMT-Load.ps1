3c92e0c0b39699d4543c0cb7725c7d4560f2bb44
2600Pz <105690957+2600Pz@users.noreply.github.com>
2025-07-07 09:28:35 -0600

    Update USMT-Load.ps1
    Added a few checks

--- USMT-Load.ps1
+++ USMT-Load.ps1
@@ -1,7 +1,15 @@
-$user = (Read-host -Prompt "Enter the username to load")
-
-New-PSDrive -name "home" -PSProvider FileSystem -Root "\\fs04\homes$\watsoj340" | Out-Null
-
+if (!(Test-Path "home:\usmt")) {
+    New-PSDrive -name "home" -PSProvider FileSystem -Root "\\fs04\homes$\watsoj340" | Out-Null
+}
 Set-Location home:\usmt | Out-Null
 
-.\loadstate.exe \\fs04\homes$\watsoj340\$user /i:MigDocs.xml /i:MigUser.xml /c
+while ($true) {
+    $dataStore = (Read-host -Prompt "Enter the data store name to load")
+    if (Test-Path "\\fs04\homes$\watsoj340\$dataStore") {
+        break
+    } else {
+        Write-Host "Data store not found. Please try again." -ForegroundColor Yellow
+    }
+}
+
+.\loadstate.exe \\fs04\homes$\watsoj340\$dataStore /i:MigDocs.xml /i:MigUser.xml /c
